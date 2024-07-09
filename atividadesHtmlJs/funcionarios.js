c6c05a7737cdd809462e7b71f25893ddc15b89dc
MATEUS JAIRAN DE SOUSA RODRIGUES <MATEUS0689@DIGITALCOLLEGE.LOCAL>
2024-07-09 16:58:55 -0300

    uso de APIs

---
+++ atividadesHtmlJs/funcionarios.js
@@ -0,0 +1,29 @@
+let funcionarios = [{
+    nome:"Mateus",
+    idade:24,
+    cargo: "CEO",
+    salario: 100.000,
+    img: "https://th.bing.com/th/id/OIP.lygNuKyE3SO_9nDG5tCYmwHaK_?rs=1&pid=ImgDetMain"
+},
+{
+    nome:"Ruan",
+    idade: 23,
+    cargo: "Gerente de Projetos",
+    salario: 50.000,
+    img:"https://th.bing.com/th/id/R.377cad200f681eba368bdc2bfe71334c?rik=FlJi07zvGP%2bh5Q&riu=http%3a%2f%2fimages.wikia.com%2fnintendo%2fen%2fimages%2farchive%2f4%2f44%2f20120820000627!Luigi-Icon.png&ehk=%2fxFC%2feCyuicaq1As0pTsgH2UmCcG3cS2jqh9QYqGs%2fo%3d&risl=&pid=ImgRaw&r=0"
+},
+
+{
+    nome:"Sara",
+    idade: 22,
+    cargo: "Gestora Fincanceira",
+    salario: 80.000,
+    img: "https://th.bing.com/th/id/OIP.x6Zuxq2QPF7tEg364v56VgHaHa?rs=1&pid=ImgDetMain"
+}
+
+]    
+
+
+array.forEach(funcionarios => {
+    
+});
