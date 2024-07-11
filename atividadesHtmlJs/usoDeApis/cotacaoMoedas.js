fc1de636454a18c56892766ffebceea3c3016246
MATEUS JAIRAN DE SOUSA RODRIGUES <MATEUS0689@DIGITALCOLLEGE.LOCAL>
2024-07-11 16:13:23 -0300

    criacao de um app de clima utilizando uma api

--- atividadesHtmlJs/usoDeApis/cotacaoMoedas.js
+++ atividadesHtmlJs/usoDeApis/cotacaoMoedas.js
@@ -1,40 +1,43 @@
-async function converterMoeda( codigoMoeda){
+async function converterMoeda(codigoMoeda) {
     let resposta = await fetch(`https://economia.awesomeapi.com.br/last/${codigoMoeda}-BRL`)
+
     let cotacao = await resposta.json()
 
-    console.log(await cotacao)
+    console.log( await cotacao)
 
-    montarTela(await cotacao)
+    montarTela( await cotacao[codigoMoeda + 'BRL'])
+}
 
-    }
+function montarTela(objeto){
 
-function montarTela(objeto[codigoMoeda + "BRL]){
-    let conversaoMoedaDiv = document.getElementById("conversaoMoeda")
+    let conversaoMoedaDiv = document.getElementById('conversaoMoeda')
 
-    let titulo = document.getElementById("h1")
+    conversaoMoedaDiv.innerHTML = ''
+
+    let titulo = document.createElement('h1')
     titulo.innerHTML = objeto.name
 
-    let valor = document.createElement("p")
-    let valorOriginalReal = document.getElementById("valor").value
-    valor.innerHTML = R$ ${valorOriginalReal}
+    let valor = document.createElement('p')
+    let valorOriginalReal = document.getElementById('valor').value
+    valor.innerHTML = `R$ ${valorOriginalReal}`
 
-    let valorConvertido = document.createElement("p")
-    valorConvertido.innerHTML = ` ${objeto.code}  ${valorOriginalReal/objeto}`
+    let valorConvertido = document.createElement('p')
+    valorConvertido.innerHTML = `${objeto.code} ${valorOriginalReal / objeto.bid}`
 
     conversaoMoedaDiv.appendChild(titulo)
     conversaoMoedaDiv.appendChild(valor)
     conversaoMoedaDiv.appendChild(valorConvertido)
 
-    let dataCotacao = document.createElement("h5")
+    let dataCotacao = document.createElement('h5')
     dataCotacao.innerHTML = objeto.create_date
-    conversaoMoedaDiv.append(dataCotacao)
-
-    let btnUsd = document.getElementById("usd")
-
-    btnUsd.addEventListener("click", ()=>{converterMoeda("USD")})
-
-    btnEur = document.getElementById("eur")
-
-    btnEur.addEventListener("click", ()=>converterMoeda("EUR"))
-
+    conversaoMoedaDiv.appendChild(dataCotacao)
 }
+
+let btnUsd = document.getElementById('usd')
+btnUsd.addEventListener('click', ()=>{converterMoeda('USD')})
+
+let btnEur = document.getElementById('eur')
+btnEur.addEventListener('click', ()=>{converterMoeda('EUR')})
+
+let btnBtc = document.getElementById('btc')
+btnBtc.addEventListener('click', ()=>{converterMoeda('BTC')})
